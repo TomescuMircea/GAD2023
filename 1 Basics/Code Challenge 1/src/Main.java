aae2ee3a92d27c48bc90aa4d7026fb8228a886aa
Tomescu Mircea-Andrei <tomircea31@yahoo.com>
2023-04-23 18:17:04 +0300

    Update Main.java

--- 1 Basics/Code Challenge 1/src/Main.java
+++ 1 Basics/Code Challenge 1/src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=1;i<=100;i++)
-        {
-            if(i%3==0 || i%5==0) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 if (i % 3 == 0)
                     System.out.printf("Fizz");
                 if (i % 5 == 0)
                     System.out.printf("Buzz");
-            }
-            else if(i%7==0)
+            } else if (i % 7 == 0)
                 System.out.printf("Rizz");
-            else if(i%11==0)
+            else if (i % 11 == 0)
                 System.out.printf("Jazz");
-            else System.out.printf("%d",i);
+            else System.out.printf("%d", i);
             System.out.printf(",");
         }
     }
